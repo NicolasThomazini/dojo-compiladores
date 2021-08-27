a44ae6863347b7cfeb2c6b17992b5f8d4bb18c09
Lara-Vitoria <larinhavitoria12@gmail.com>
2021-08-26 21:52:19 -0300

    segundo comentario

--- Desktop/Compiladores/dojo/src/Scanner.java
+++ Desktop/Compiladores/dojo/src/Scanner.java
@@ -1,5 +1,41 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Scanner {
+
+    private char [] content; 
+
+    public Scanner(String filename) {
+        try {
+
+            String txtAnalise;
+            txtAnalise = new String(Files.readAllBytes(Paths.get(filename), StandardCharsets.UTF_8));
+            content = txtAnalise.toCharArray();
+        }
+        catch ( Exception ex ){
+
+            ex.printStackTrace();
+        }
     }
+
+    private boolean isNumber( char c ){
+
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isChar( char c ){
+
+        return c >= 'a' && c <= 'z';
+    }
+
+    private boolean isOperator( char c ){
+
+        return c == '>' || c == '<' || c == '=' || c == '!';
+    }
+
+    private boolean isSpace( char c ){
+
+        return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+    }  
 }
